e0c7aefbe9c452281a344d53ac182fb2bae6c375
sohail <coderu340@gmail.com>
2025-10-02 19:19:08 +0500

    addnewfeature

---
+++ Controller/api.js
@@ -0,0 +1,8 @@
+const addNotes = (req,res) => {
+     try {
+        
+        res.status(200).json('Message is done')
+     } catch (error) {
+         res.status(400).json({message:error})
+     }
+}
